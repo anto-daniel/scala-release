00513b69f2f1baa36856ad1f7099dc0558da902c
Anto Daniel <anto.daniel@gmail.com>
2019-09-21 14:06:46 -0400

    modifying startup file...

--- jobs/scala/templates/ctl.sh
+++ jobs/scala/templates/ctl.sh
@@ -11,7 +11,7 @@ case $1 in
     mkdir -p $LOG_DIR
 
     echo $$ > $PIDFILE
-    exec /var/vcap/packages/scala/usr/bin/scalac /var/vcap/packages/scala/ScalaExample.scala >>$LOG_DIR/scala.stdout.log 2>>$LOG_DIR/scala.stderr.log
+    exec /var/vcap/packages/scala/usr/share/scala/bin/scalac /var/vcap/packages/scala/ScalaExample.scala >>$LOG_DIR/scala.stdout.log 2>>$LOG_DIR/scala.stderr.log
     #exec /var/vcap/packages/scala/usr/bin/scala ScalaExample >>$LOG_DIR/scala.stdout.log 2>>$LOG_DIR/scala.stderr.log
     ;;
 
